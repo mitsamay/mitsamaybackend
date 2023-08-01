a457550d6b3a2e185160aed3ea20b8d4001b8042
Mitsamay Keotheuankham <mitsamay@gmail.com>
2023-08-01 17:38:32 +0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const ErrorHandler = require("./middleware/error");
+// const ErrorHandler = require("./middleware/error");
 const app = express();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
+// const cookieParser = require("cookie-parser");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const path = require("path");
 
-app.use(cors({
-  origin: 'https://eshop-tutorial-cefl.vercel.app',
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'https://eshop-tutorial-cefl.vercel.app',
+//   credentials: true
+// }));
 
-app.use(express.json());
-app.use(cookieParser());
-app.use("/", express.static(path.join(__dirname,"./uploads")));
-app.use("/test", (req, res) => {
-  res.send("Hello world!");
-});
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use("/", express.static(path.join(__dirname,"./uploads")));
+// app.use("/test", (req, res) => {
+//   res.send("Hello world!");
+// });
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
@@ -28,29 +28,29 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // import routes
-const user = require("./controller/user");
-const shop = require("./controller/shop");
-const product = require("./controller/product");
-const event = require("./controller/event");
-const coupon = require("./controller/coupounCode");
-const payment = require("./controller/payment");
-const order = require("./controller/order");
-const conversation = require("./controller/conversation");
-const message = require("./controller/message");
-const withdraw = require("./controller/withdraw");
+// const user = require("./controller/user");
+// const shop = require("./controller/shop");
+// const product = require("./controller/product");
+// const event = require("./controller/event");
+// const coupon = require("./controller/coupounCode");
+// const payment = require("./controller/payment");
+// const order = require("./controller/order");
+// const conversation = require("./controller/conversation");
+// const message = require("./controller/message");
+// const withdraw = require("./controller/withdraw");
 
-app.use("/api/v2/user", user);
-app.use("/api/v2/conversation", conversation);
-app.use("/api/v2/message", message);
-app.use("/api/v2/order", order);
-app.use("/api/v2/shop", shop);
-app.use("/api/v2/product", product);
-app.use("/api/v2/event", event);
-app.use("/api/v2/coupon", coupon);
-app.use("/api/v2/payment", payment);
-app.use("/api/v2/withdraw", withdraw);
+// app.use("/api/v2/user", user);
+// app.use("/api/v2/conversation", conversation);
+// app.use("/api/v2/message", message);
+// app.use("/api/v2/order", order);
+// app.use("/api/v2/shop", shop);
+// app.use("/api/v2/product", product);
+// app.use("/api/v2/event", event);
+// app.use("/api/v2/coupon", coupon);
+// app.use("/api/v2/payment", payment);
+// app.use("/api/v2/withdraw", withdraw);
 
 // it's for ErrorHandling
-app.use(ErrorHandler);
+// app.use(ErrorHandler);
 
 module.exports = app;
